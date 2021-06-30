2d69a40632c2c49b7f1afd77704aef5260330077
bebofofum <bri.boehm@gmail.com>
2021-06-30 14:30:13 -0400

    A temporary build out of user actions and reducers. To get refactored soon.

---
+++ dislingo-client-app/src/features/userSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// this looks like its part of my reducer with the intital state defined
+// this is really bad coding. confusing. jumbled. I hate this setup
+// lets go back and clean this shit up later
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+     user: null
+  },
+  reducers: {
+    loginUser: (state,action) => {
+      state.user += action.payload;
+    },
+    logoutUser: (state,action) => {
+      state.user = {};
+    }
+  }
+})
+
+export const { loginUser, logoutUser } = userSlice.actions;
+
+
+export const selectUser = (state) => state.user.user;
+
+export default userSlice.reducer;
+
+
+
