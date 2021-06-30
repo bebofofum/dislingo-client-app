fcbe9dde9bea426c2b9963ad73910f586c91b56b
bebofofum <bri.boehm@gmail.com>
2021-06-30 14:39:17 -0400

    Created conditional rendering function for checking user existence. And components for sidebar and hchatpanel.

--- dislingo-client-app/src/App.js
+++ dislingo-client-app/src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 import './App.css';
 import Sidebar from './components/Sidebar';
 import ChatPanel from './components/ChatPanel';
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser); //this is a hook. equivalent to mapstatetoprops and connect
+
+  function checkUserRender() {
+    return user ? (
+      <>
+       <Sidebar /> 
+       <ChatPanel />
+      </>
+    ) : (
+      <h2>You need to login</h2>
+    )
+  }
+
+ 
+
   return (
     <div className="app">
-      {/* Sidebar component */}
-      <Sidebar />    
-
-      {/* Sidebar component */}
-      <ChatPanel />
-  
+     {checkUserRender()}
     </div>
   );
 }
