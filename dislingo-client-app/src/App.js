74b93821d26225964ce230c39652f654f696a5db
bebofofum <bri.boehm@gmail.com>
2021-06-30 20:55:43 -0400

    Fixed useeffect hook to dispatch user actions on conditionals.

--- dislingo-client-app/src/App.js
+++ dislingo-client-app/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import ChatPanel from './components/ChatPanel';
-import { selectUser } from './features/userSlice';
+import Login from './components/Login';
+import { selectUser, loginUser, logoutUser } from './features/userSlice';
+import { auth } from './firebase';
 
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser); //this is a hook. equivalent to mapstatetoprops and connect
+
+  useEffect(() => {  //where is the user being passed into the firebase authstate method? is it automatically pulled in from firebase?
+    auth.onAuthStateChanged((permittedUser) => { //documentation seems to suggest firebase adds current user when called with parameter
+      console.log('user is', permittedUser.displayName)
+
+      if (permittedUser) {
+         dispatch(loginUser({
+           uid: permittedUser.uid,
+           photo: permittedUser.photoURL,
+           email: permittedUser.email,
+           name: permittedUser.displayName
+         }))
+      } else {
+        dispatch(logoutUser())
+      }
+
+    });
+  }, [dispatch]); //need to look into how useEffect hook works.
 
   function checkUserRender() {
     return user ? (
@@ -15,7 +36,7 @@ function App() {
        <ChatPanel />
       </>
     ) : (
-      <h2>You need to login</h2>
+      <Login />
     )
   }
 
