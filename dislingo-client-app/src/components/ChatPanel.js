ff3fddaabf840ab2896bac3409ec8673769e3c57
bebofofum <bri.boehm@gmail.com>
2021-06-30 20:56:29 -0400

    Removed unnecessary imports.

--- dislingo-client-app/src/components/ChatPanel.js
+++ dislingo-client-app/src/components/ChatPanel.js
@@ -3,8 +3,6 @@ import '../Chat.css';
 import ChatHeader from './ChatHeader';
 import ChatInput from './ChatInput';
 import ChatMessages from './ChatMessages';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import AddIcon from '@material-ui/icons/Add';
 
 
 const ChatPanel = () => {
