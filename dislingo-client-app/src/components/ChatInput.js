6112cdce2f9f2db24c1b8ddeec1ae21025b1f2ab
bebofofum <bri.boehm@gmail.com>
2021-07-21 18:02:17 -0400

    Adding useState to make use of state data

--- dislingo-client-app/src/components/ChatInput.js
+++ dislingo-client-app/src/components/ChatInput.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Chat.css';
 import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
 import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
 import GifIcon from '@material-ui/icons/Gif';
 import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 
-const ChatInput = () => {
+const ChatInput = ({ channelId, channelName }) => {
+  const [input, setInput] = useState("");
+
+  const handleOnChange = (e) => setInput(e.target.value)
+
+
   return (
     <section className="chat-input">
       <AddCircleRoundedIcon fontSize="large" />
       <form>
         <input 
         className=""
-        placeholder="Message #TESTCHANNEL" 
+        disabled={!channelId}
+        onChange={handleOnChange}
+        placeholder={`Message #${channelName}`} 
         type="text"
         name="message" 
+        value={input}
         />
         <button 
+        disabled={!channelId}
         className="chat-input-submit-button"
         type="submit">Send Message</button>
       </form>
