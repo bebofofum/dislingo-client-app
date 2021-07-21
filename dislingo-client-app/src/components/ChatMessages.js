c9eef19918cd9debfa97eed82df56939b28b295b
bebofofum <bri.boehm@gmail.com>
2021-07-21 18:04:42 -0400

    Making use of hook to dynamically display message state data.

--- dislingo-client-app/src/components/ChatMessages.js
+++ dislingo-client-app/src/components/ChatMessages.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Chat.css';
 import ChatMessageItem from './MessageItem';
 
 const ChatMessages  = () => {
+
+  const [messages, setMessages] = useState([])
+
+
   return(
+
+
     <section className="chat-area">
       <div className="chat-messages">
         <ChatMessageItem />
