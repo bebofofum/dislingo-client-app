84779701a3a75312f1cafb92a60095a17ba01b50
bebofofum <bri.boehm@gmail.com>
2021-07-21 18:08:48 -0400

    Getting familiar with useDispatch hook to update channelId and channelname in store

--- dislingo-client-app/src/components/SidebarChannelList.js
+++ dislingo-client-app/src/components/SidebarChannelList.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setChannelDetails } from '../features/appSlice';
 import "../SidebarChannel.css";
 
 
 const SidebarChannelList = ({ id, channelName }) => {  //passing props to SidebarChannel from parent, destructuring here
+  const dispatch = useDispatch();
+
   return (
     <section className="sidebar-channel">
-      <h4>
+      <h4 
+        onClick={() => 
+          dispatch(
+            setChannelDetails( {
+              channelId: id,
+              channelName: channelName
+          } ))}
+      >
         <span className="sidebar-channel-hash">#</span>
         {channelName}</h4>
     </section>
