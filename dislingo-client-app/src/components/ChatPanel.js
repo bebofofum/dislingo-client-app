6b1f51eae047936cf0c7aa24c75f5b6ac5724022
bebofofum <bri.boehm@gmail.com>
2021-07-21 18:06:00 -0400

    Getting familiar with useSelector hooks to grab channelId and channelName properties

--- dislingo-client-app/src/components/ChatPanel.js
+++ dislingo-client-app/src/components/ChatPanel.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import '../Chat.css';
+
 import ChatHeader from './ChatHeader';
 import ChatInput from './ChatInput';
 import ChatMessages from './ChatMessages';
 
+import { selectChannelId, selectChannelName } from '../features/appSlice';
+import { selectUser } from '../features/userSlice';
+
 
 const ChatPanel = () => {
+  const channelId = useSelector(selectChannelId);
+  const channelName = useSelector(selectChannelName);
+  const user = useSelector(selectUser);
+
+
+
   return (
     <section className="chat-panel">
-      <ChatHeader />
+      <ChatHeader channelName={channelName} />
       <ChatMessages />    
-      <ChatInput />
+      <ChatInput channelId={channelId} channelName={channelName} />
 
     </section>
   )
